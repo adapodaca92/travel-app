985356dc290d53d988b620b3a90fdec3d72d9bdd
Anthony Apodaca <53668339+adapodaca92@users.noreply.github.com>
2021-11-13 12:32:30 -0800

    Initial Commit

---
+++ client/src/components/Register.jsx
@@ -0,0 +1,54 @@
+import "./register.css";
+import { Room } from "@material-ui/icons";
+import { useState, useRef } from "react";
+import axios from "axios";
+import { Cancel } from "@material-ui/icons";
+
+function Register({ setShowRegister }) {
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newUser = {
+      username: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+    try {
+      await axios.post("/users/register", newUser);
+      setError(false);
+      setSuccess(true);
+    } catch (err) {
+      setError(true);
+    }
+  };
+
+  return (
+    <div className="registerContainer">
+      <div className="logo">
+        <Room />
+        Travel Log
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="username" ref={nameRef}></input>
+        <input type="email" placeholder="email" ref={emailRef}></input>
+        <input type="password" placeholder="password" ref={passwordRef}></input>
+        <button className="registerButton">Register</button>
+        {success && (
+          <span className="success">Registration successful!</span>
+        )}{" "}
+        {error && <span className="failure">Something went wrong!</span>}
+      </form>
+      <Cancel
+        className="registerCancel"
+        onClick={() => setShowRegister(false)}
+      />
+    </div>
+  );
+}
+
+export default Register;
